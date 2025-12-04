83e92fb3de905c5fb14145d886aa128ed8f8166d
AHADI JOEL <joeahangila0@gmail.com>
2025-12-04 13:07:54 +0300

    Delete handlers directory

--- handlers/__init__.py
+++
@@ -1,6 +0,0 @@
-from .user_handlers import user_router
-from .admin_handlers import admin_router
-from .game_handlers import game_router
-from .notification_handlers import notification_router
-
-__all__ = ['user_router', 'admin_router', 'game_router', 'notification_router']
